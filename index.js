243d46723416c9d723fb93db93947621df41d90c
nishaNayak4 <nishanayak868@gmail.com>
2026-09-20 12:36:07 +0530

    Prepare app for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require("path")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
@@ -26,5 +26,5 @@ app.get('/contact', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
